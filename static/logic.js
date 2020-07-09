4c05259b6b561265a6393f2f9cc7657a9238563a
Masita <masita.mohamad@gmail.com>
2020-07-08 23:15:21 -0500

    Added app.py

--- static/logic.js
+++ static/logic.js
@@ -1,13 +1,10 @@
 // Create a map object
-var myMap = L.map("map", {
-  center: [37.09, -95.71],
-  zoom: 5
-});
+var mymap = L.map('map').setView([38.7575, -99.0100], 3);
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.mapbox-streets-v8",
+  maxZoom: 5,
+  id: "mapbox.satellite",
   accessToken: API_KEY
 }).addTo(myMap);
 
